8502f603ea2754458a0aa621269de20bd4b1e92a
Kamel <Mohammed99kamel@gmail.com>
2021-06-03 18:15:42 +0200

    Applying hash function

--- SignalFingerPrint.py
+++ SignalFingerPrint.py
@@ -10,6 +10,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QDialog,QApplication,QFileDialog
 from PyQt5.uic import loadUi
 import os
+from librosa.feature.spectral import mfcc
 from numpy import disp, float16
 from scipy.io import wavfile
 from scipy.signal import spectrogram
@@ -21,7 +22,6 @@ from imagehash import hex_to_hash
 import glob
 from glob import glob
 import librosa.display
-
 from PIL import Image
 import imagehash
 from imagehash import hex_to_hash
@@ -31,7 +31,12 @@ import numpy as np
 
 class Ui_MainWindow( QDialog):
     def setupUi(self, MainWindow):
-        self.filePath='./Songs_wav/'
+        
+        self.inserted_song=''
+        self.inserted_song_hash=0
+        self.filePath='./Songs_wav'
+        self.comparison_difference=[]
+        self.similiraties = []
         self.listSongsPaths=[]
         self.spectograms=[]
         self.data=[]
@@ -92,9 +97,11 @@ class Ui_MainWindow( QDialog):
        
         
     def browsefiles(self,button):
-        fname=QFileDialog.getOpenFileName(self, 'Open file','C:/')
+        fname=QFileDialog.getOpenFileName(self, 'Open file','./')
         if button==self.Song1_Browse:
             self.Song1_FileName.setText(fname[0])
+            self.inserted_song = fname[0]
+            print(self.inserted_song)
         else:
             self.Song2_FileName.setText(fname[0])
    
@@ -102,63 +109,95 @@ class Ui_MainWindow( QDialog):
     def folderSongsPaths(self):
         for song in os.listdir(self.filePath):
             self.listSongsPaths.append(os.path.join(self.filePath,song))
+        print(self.listSongsPaths)
         return 0
     
-    def Hash(self,data)->str:
+    def Hash(self,data):
         image=Image.fromarray(data)
-        return imagehash.phash(image, hash_size=16)._str_()
+        return imagehash.phash(image, hash_size=16).__str__()
 
     
     def creatingSongsSpectrogram(self):
-        self.folderSongsPaths() 
-        print(self.listSongsPaths)
-        print(len(self.listSongsPaths))
-        for song in self.listSongsPaths:
-            sampleRate,data1=wavfile.read(song)
-            self.SampleRateList.append(sampleRate)
-            print('11111111111')
-            if len(data1)>60*sampleRate:
-                data1=data1[0:60*sampleRate]
-            self.data.append(data1)    
-            frequence,time,specgram=spectrogram(data1,sampleRate)
-            self.spectograms.append(specgram)
+        pass
+        # self.folderSongsPaths() 
+        # print(self.listSongsPaths)
+        # print(len(self.listSongsPaths))
+        # for song in self.listSongsPaths:
+        #     sampleRate,data1=wavfile.read(song)
+        #     self.SampleRateList.append(sampleRate)
+        #     # print('11111111111')
+        #     if len(data1)>60*sampleRate:
+        #         data1=data1[0:60*sampleRate]
+        #     self.data.append(data1)    
+        #     frequence,time,specgram=spectrogram(data1,sampleRate)
+        #     self.spectograms.append(specgram)
+            
+            
             # librosa.display.specshow(specgram)
             # plt.savefig('./Spectrogram/F1_Song1'+str(song).zfill(3) +'.png')
             # image=Image.open('./Spectrogram/F1_Song1'+str(song).zfill(3) +'.png')
             # specHashed=imagehash.phash(image)
-            self.spectroHashList.append(self.Hash(specgram))
-            print('specHash',self.spectroHashList)
+            # self.spectroHashList.append(self.Hash(specgram))
+            # print('specHash',self.spectroHashList)
             # self.spectroHashList.append(self.Hash(specgram.astype('float16')))
     
+    def maps(self, inputValue: float, inMin: float, inMax: float, outMin: float, outMax: float):
+        slope = (outMax-outMin) / (inMax-inMin)
+        return outMin + slope*(inputValue-inMin)
 
-    def extractFeatures(self):
-        #  for i in range(len(self.data)):
-            # # filename = librosa.util.example_audio_file()
-            self.data_dir='./Songs_wav/'
-            self.SpectroGram=glob(self.data_dir+'*.wav')
-            for i in range(len(self.SpectroGram)):
-                data,sample_rate=librosa.load(str(self.SpectroGram[i]),duration=60.0)
-                feature=librosa.feature.mfcc(y=data.astype('float64'),sr=sample_rate)
-                # librosa.display.specshow(feature)
-                # plt.savefig('./Feature1/F1_Song1'+str(i).zfill(3) +'.png')
-                # file='./Feature1/F1_Song1'+str(i).zfill(3) +'.png'
-                # image=Image.open(file)
-                # FeaturedHashed=imagehash.phash(image)
-                self.featureHashList1.append(self.Hash(feature))
-                feature=[]
-                print('feature ',self.featureHashList1)
-            # features=[librosa.feature.melspectrogram(y=self.data[i],sr=self.SampleRateList[i],S=self.spectograms[i]), 
-            # librosa.feature.mfcc(y=self.data[i].astype('float64'),sr=self.SampleRateList[i]),
-            # librosa.feature.chroma_stft(y=self.data[i],sr=self.SampleRateList[i],S=self.spectograms[i])]                   
-            # self.featuresList.append(features)
-            # features=[]
-        # print('features ',self.featuresList)
-  
+    def extractFeatures(self):    
+        # print('FFFFFFFFFFFFFFFFFFFFF')
+        self.folderSongsPaths() 
+        
+        # self.data_dir='./Songs_wav/'
+        # self.SpectroGram=glob(self.data_dir+'*.wav')
+        count =0
+        for song in self.listSongsPaths:
+            data,sample_rate=librosa.load(song, duration = 60)
+            
+            mfccs = librosa.feature.mfcc(data, sr=sample_rate)
+            # print(mfccs.shape)
+            
+            X = librosa.stft(data)
+            Xdb = librosa.amplitude_to_db(abs(X))
+            plt.figure(figsize=(14, 5))
+            librosa.display.specshow(Xdb, sr=sample_rate, x_axis='time', y_axis='hz') 
+            #If to pring log of frequencies  
+            #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+            plt.colorbar()
+            plt.savefig('./Spectrogram1/F1_Song1_'+str(count).zfill(3) +'.png')
+            
+            
+            librosa.display.specshow(mfccs)
+            plt.savefig('./Feature1/F1_Song1_'+str(count).zfill(3) +'.png')
+            count+=1
+        
+            
+            self.featureHashList1.append(self.Hash(mfccs))
+            print('Loading ', count)
+            print(self.Hash(mfccs))
+            feature=[]
+        print('feature ',self.featureHashList1)
+        
+        #new song
+        data,sample_rate=librosa.load(self.inserted_song , duration = 60)
+        mfccs = librosa.feature.mfcc(data, sr=sample_rate)
+        self.inserted_song_hash = self.Hash(mfccs)
+        print(self.inserted_song_hash)
+        
+        
+        for song in range(len(self.listSongsPaths)):
+            self.comparison_difference.append(hex_to_hash(self.inserted_song_hash) - hex_to_hash(self.featureHashList1[song]))
+            self.similiraties.append((1 - self.maps(self.comparison_difference[song], 0, 255, 0, 1) )* 100)
+        
+        for song in range(len(self.listSongsPaths)):
+            print(self.listSongsPaths[song], ':', self.similiraties[song])
 
+        
 
     def reading_and_hashing(self):
-        self.creatingSongsSpectrogram()
-        print('Finished Spectro Function')
+        # self.creatingSongsSpectrogram()
+        # print('Finished Spectro Function')
 
         self.extractFeatures()
         print('Finished Feature Function')
